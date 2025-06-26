fe5545daa5bda00ee5781cab151c8bc93dad9a50
Ashish jha <ajha5645@gmail.com>
2025-06-27 02:26:58 +0530

    Refactor Roles class: fix equals method implementation and adjust formatting

--- src/main/java/com/entity/Roles.java
+++ src/main/java/com/entity/Roles.java
@@ -1,50 +1,48 @@
 package com.crawlforge.model;
 
-import jakarta.persistence.*;
-import lombok.*;
+        import jakarta.persistence.*;
+        import lombok.*;
 
-import javax.management.relation.Role;
-import java.util.HashSet;
-import java.util.Set;
-import com.crawlforge.model.User;
+        import java.util.HashSet;
+        import java.util.Set;
 
-@Entity
-@Table(name = "roles")
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-@ToString(exclude = "users") // Avoid circular reference
-public class Roles {
+        @Entity
+        @Table(name = "roles")
+        @Getter
+        @Setter
+        @NoArgsConstructor
+        @AllArgsConstructor
+        @Builder
+        @ToString(exclude = "users") // Avoid circular reference
+        public class Roles {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "role_id")
-    @Setter(AccessLevel.NONE)
-    private Long roleId;
+            @Id
+            @GeneratedValue(strategy = GenerationType.IDENTITY)
+            @Column(name = "role_id")
+            @Setter(AccessLevel.NONE)
+            private Long roleId;
 
-    @Column(name = "name", unique = true, nullable = false, length = 50)
-    private String name;
+            @Column(name = "name", unique = true, nullable = false, length = 50)
+            private String name;
 
-    @Column(name = "description", length = 255)
-    private String description;
+            @Column(name = "description", length = 255)
+            private String description;
 
-    @ManyToMany(mappedBy = "roles")
-    @Builder.Default
-    private Set<User> users = new HashSet<>();
+            @ManyToMany(mappedBy = "roles")
+            @Builder.Default
+            private Set<User> users = new HashSet<>();
 
-    // Manual equals and hashCode
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Roles)) return false;
-        Role role = (Roles) o;
-        return roleId != null && roleId.equals(role.roleId);
-    }
+            // Manual equals and hashCode
+            @Override
+            public boolean equals(Object o) {
+                if (this == o) return true;
+                if (!(o instanceof Roles)) return false;
+                Roles role = (Roles) o;
+                return roleId != null && roleId.equals(role.roleId);
+            }
 
-    @Override
-    public int hashCode() {
-        return getClass().hashCode();
-    }
-}
+            @Override
+            public int hashCode() {
+                return getClass().hashCode();
+            }
+        }
