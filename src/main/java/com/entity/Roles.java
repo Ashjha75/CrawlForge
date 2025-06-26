5d75baf3320ec56a5afacc465575060846347711
Ashish jha <ajha5645@gmail.com>
2025-06-27 01:06:47 +0530

    Add User and Roles entities with Hibernate mapping; integrate Lombok and update POM dependencies

---
+++ src/main/java/com/entity/Roles.java
@@ -0,0 +1,48 @@
+package com.crawlforge.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString(exclude = "users") // Avoid circular reference
+public class Roles {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    @Setter(AccessLevel.NONE)
+    private Long roleId;
+
+    @Column(name = "name", unique = true, nullable = false, length = 50)
+    private String name;
+
+    @Column(name = "description", length = 255)
+    private String description;
+
+    @ManyToMany(mappedBy = "roles")
+    @Builder.Default
+    private Set<User> users = new HashSet<>();
+
+    // Manual equals and hashCode
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Role)) return false;
+        Role role = (Role) o;
+        return roleId != null && roleId.equals(role.roleId);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
