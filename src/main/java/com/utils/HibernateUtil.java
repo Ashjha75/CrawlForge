2c51131dedab310c00a155caead0dde509a615d5
Ashish jha <ajha5645@gmail.com>
2025-06-28 00:19:09 +0530

    Add .gitignore and refactor Hibernate configuration to load database properties from db.properties

--- src/main/java/com/utils/HibernateUtil.java
+++ src/main/java/com/utils/HibernateUtil.java
@@ -3,13 +3,30 @@ package com.utils;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.io.InputStream;
+import java.util.Properties;
+
 public class HibernateUtil {
     private static final SessionFactory sessionFactory = buildSessionFactory();
 
     private static SessionFactory buildSessionFactory() {
         try {
-            // looks for /hibernate.cfg.xml on classpath
-            return new Configuration().configure().buildSessionFactory();
+            Configuration configuration = new Configuration().configure();
+
+            Properties props = new Properties();
+            try (InputStream input = HibernateUtil.class.getClassLoader().getResourceAsStream("db.properties")) {
+                if (input == null) {
+                    throw new RuntimeException("db.properties not found in classpath");
+                }
+                props.load(input);
+            }
+
+            configuration.setProperty("hibernate.connection.driver_class", props.getProperty("db.driver"));
+            configuration.setProperty("hibernate.connection.url", props.getProperty("db.url"));
+            configuration.setProperty("hibernate.connection.username", props.getProperty("db.username"));
+            configuration.setProperty("hibernate.connection.password", props.getProperty("db.password"));
+
+            return configuration.buildSessionFactory();
         } catch (Throwable ex) {
             System.err.println("Initial SessionFactory creation failed: " + ex);
             throw new ExceptionInInitializerError(ex);
