d34e862091e496f1bcf93331b7729c1402ee7f7e
Ashish jha <ajha5645@gmail.com>
2025-06-27 17:52:31 +0530

    Add browserconfig.xml, JSP pages for `dashboard` and `error`, and database properties file

--- src/main/java/com/utils/HibernateUtil.java
+++ src/main/java/com/utils/HibernateUtil.java
@@ -1,40 +1,22 @@
 package com.utils;
 
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
 
 public class HibernateUtil {
-
-    private static SessionFactory sessionFactory;
+    private static final SessionFactory sessionFactory = buildSessionFactory();
 
     private static SessionFactory buildSessionFactory() {
         try {
-            // Create the SessionFactory from hibernate.cfg.xml
-            Configuration configuration = new Configuration();
-            configuration.configure("hibernate.cfg.xml");
-
-            System.out.println("Hibernate Configuration loaded");
-
-            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
-                    .applySettings(configuration.getProperties()).build();
-
-            System.out.println("Hibernate serviceRegistry created");
-
-            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
-            return sessionFactory;
-
+            // looks for /hibernate.cfg.xml on classpath
+            return new Configuration().configure().buildSessionFactory();
         } catch (Throwable ex) {
-            System.err.println("Initial SessionFactory creation failed." + ex);
+            System.err.println("Initial SessionFactory creation failed: " + ex);
             throw new ExceptionInInitializerError(ex);
         }
     }
 
     public static SessionFactory getSessionFactory() {
-        if (sessionFactory == null) {
-            sessionFactory = buildSessionFactory();
-        }
         return sessionFactory;
     }
 
