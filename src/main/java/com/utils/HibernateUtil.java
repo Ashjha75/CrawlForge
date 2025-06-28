53b76ae70956dc8bb9bddb212172c60a472d5b0e
Ashish jha <ajha5645@gmail.com>
2025-06-28 23:27:38 +0530

    Refactor Hibernate configuration for improved environment variable support and add missing entity mappings

--- src/main/java/com/utils/HibernateUtil.java
+++ src/main/java/com/utils/HibernateUtil.java
@@ -1,44 +1,49 @@
-package com.crawlforge.util;
+package com.service;
 
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
 
 public class HibernateUtil {
-
-    private static SessionFactory sessionFactory;
+    private static final SessionFactory sessionFactory = buildSessionFactory();
 
     private static SessionFactory buildSessionFactory() {
         try {
-            // Create the SessionFactory from hibernate.cfg.xml
             Configuration configuration = new Configuration();
-            configuration.configure("hibernate.cfg.xml");
 
-            System.out.println("Hibernate Configuration loaded");
+            // Set properties using environment variables from Render
+            configuration.setProperty("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver");
+            configuration.setProperty("hibernate.connection.url", System.getenv("DB_URL"));
+            configuration.setProperty("hibernate.connection.username", System.getenv("DB_USER"));
+            configuration.setProperty("hibernate.connection.password", System.getenv("DB_PASSWORD"));
 
-            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
-                    .applySettings(configuration.getProperties()).build();
+            configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+            configuration.setProperty("hibernate.hbm2ddl.auto", "update");
+            configuration.setProperty("hibernate.show_sql", "true");
+            configuration.setProperty("hibernate.format_sql", "true");
 
-            System.out.println("Hibernate serviceRegistry created");
+            // HikariCP settings (optional, tune as needed)
+            configuration.setProperty("hibernate.connection.provider_class", "org.hibernate.hikaricp.internal.HikariCPConnectionProvider");
+            configuration.setProperty("hibernate.hikari.minimumIdle", "5");
+            configuration.setProperty("hibernate.hikari.maximumPoolSize", "20");
+            configuration.setProperty("hibernate.hikari.idleTimeout", "300000");
+            configuration.setProperty("hibernate.hikari.maxLifetime", "1200000");
+            configuration.setProperty("hibernate.hikari.connectionTimeout", "20000");
 
-            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
-            return sessionFactory;
+            // Add annotated entity classes
+            configuration.addAnnotatedClass(com.entity.User.class);
+            configuration.addAnnotatedClass(com.entity.Role.class);
+            configuration.addAnnotatedClass(com.entity.CrawlSession.class);
+            configuration.addAnnotatedClass(com.entity.Page.class);
+            configuration.addAnnotatedClass(com.entity.Link.class);
+            configuration.addAnnotatedClass(com.entity.Keyword.class);
 
+            return configuration.buildSessionFactory();
         } catch (Throwable ex) {
-            System.err.println("Initial SessionFactory creation failed." + ex);
             throw new ExceptionInInitializerError(ex);
         }
     }
 
     public static SessionFactory getSessionFactory() {
-        if (sessionFactory == null) {
-            sessionFactory = buildSessionFactory();
-        }
         return sessionFactory;
-    }
-
-    public static void shutdown() {
-        getSessionFactory().close();
     }
 }
