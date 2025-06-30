7f8a5cf64bba441c65c6f7140cebe6f45e6736b3
Ashish jha <ajha5645@gmail.com>
2025-06-30 14:49:47 +0530

    Refactor `UserCheckServlet`, `UserDAO`, and `web.xml` to improve formatting consistency and readability

--- src/main/java/com/utils/LocalDateTimeAdapter.java
+++ src/main/java/com/utils/LocalDateTimeAdapter.java
@@ -2,21 +2,22 @@ package com.utils;
 
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.google.gson.*;
+
 import java.lang.reflect.Type;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class LocalDateTimeAdapter implements JsonSerializer<LocalDateTime>, JsonDeserializer<LocalDateTime> {
-    
+
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-    
+
     @Override
     public JsonElement serialize(LocalDateTime localDateTime, Type type, JsonSerializationContext context) {
         return new JsonPrimitive(localDateTime.format(formatter));
     }
-    
+
     @Override
-    public LocalDateTime deserialize(JsonElement json, Type type, JsonDeserializationContext context) 
+    public LocalDateTime deserialize(JsonElement json, Type type, JsonDeserializationContext context)
             throws JsonParseException {
         return LocalDateTime.parse(json.getAsString(), formatter);
     }
